e6894100791014017fb575aeb6f24317f8b51b28
arthurns1 <js.arthurns90@gmail.com>
2026-01-20 11:44:35 -0300

    Adicionando função check_login

---
+++ backend/src/middlewares/validation/check_login.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import Jwt from "jsonwebtoken";
+
+async function check_login(req: Request, res: Response, next: NextFunction) {
+    try {
+        await Jwt.verify(
+            req.headers.authorization as string,
+            process.env.SECRET_KEY as string,
+        );
+
+        next();
+    } catch (err) {
+        res.status(401).json({
+            error_message: "Acesso negado!",
+            error: err,
+        });
+    }
+}
+
+export { check_login };
--- backend/src/routes/item.ts
+++ backend/src/routes/item.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ControllerItens } from "../controllers/controllerItens";
 import { validateItem } from "../middlewares/validation/validateItem";
 import { upload } from "../services/upload";
+import { check_login } from "../middlewares/validation/check_login";
 
 const item = Router();
 
@@ -9,12 +10,17 @@ item.get("/get_all", ControllerItens.get_all);
 item.get("/get_by_id/:id_item", ControllerItens.get_by_id_item);
 item.post(
     "/create",
+    check_login,
     upload.single("foto"),
     validateItem,
     ControllerItens.create,
 );
-item.put("/register_return/:id_item", ControllerItens.register_return);
-item.put("/update/:id_item", ControllerItens.update_by_id_item);
-item.delete("/delete/:id_item", ControllerItens.delete_by_id_item);
+item.put(
+    "/register_return/:id_item",
+    check_login,
+    ControllerItens.register_return,
+);
+item.put("/update/:id_item", check_login, ControllerItens.update_by_id_item);
+item.delete("/delete/:id_item", check_login, ControllerItens.delete_by_id_item);
 
 export { item };
